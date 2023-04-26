0b930c2865d589091bd1249a70c887f6510cf463
thanawat <everlastbw@hotmail.com>
2023-04-26 10:44:05 +0700

    update

--- src/service/Api/api.ts
+++ src/service/Api/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import IUser from "../../components/model/IUser";
+import IUser from "../../models/IUser";
+
 const BASE_URL = "http://192.168.2.62:3001/";
 export async function getUser() {
   await axios.get<Array<IUser>>(`${BASE_URL}user/getAll`);
